cb825ee8216df2fa6c534226df32a83cf036bb19
RohithSachdeva <rsachdeva@ucdavis.edu>
2021-04-12 16:56:00 -0700

    Utilized userSlice actions in app.js; starting ProfileScreen

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import HomeScreen from "./components/HomeScreen/HomeScreen.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginScreen from "./components/LoginScreen/LoginScreen";
 import { auth } from "./firebase/firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import ProfileScreen from "./components/ProfileScreen/ProfileScreen";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser); //Verify userCredentials
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         //logged in
         console.log(userAuth);
+        //dispatch an object
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
-        //logged out
+        dispatch(logout);
       }
     });
     return unsubscribe;
@@ -26,6 +38,9 @@ function App() {
           <LoginScreen />
         ) : (
           <Switch>
+            <Route path="/profile">
+              <ProfileScreen />
+            </Route>
             <Route exact path="/">
               <HomeScreen />
             </Route>
---
+++ src/components/ProfileScreen/ProfileScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ProfileScreen.css";
+
+function ProfileScreen() {
+  return (
+    <div classname="profileScreen">
+      <h1>This is the profile</h1>
+    </div>
+  );
+}
+
+export default ProfileScreen;
