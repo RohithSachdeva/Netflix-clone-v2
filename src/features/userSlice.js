b8a835465fd938051bff3c95f8d4a3d17471e422
RohithSachdeva <rsachdeva@ucdavis.edu>
2021-04-12 16:39:27 -0700

    Test Commit

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const userSlice = createSlice({
-  name: "user",
+  name: "user", //piece of state
   initialState: {
     user: null,
   },
@@ -16,10 +16,9 @@ export const userSlice = createSlice({
 });
 
 export const { login, logout } = userSlice.actions;
+//Export actions that affect the user section of the global store
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
-export const selectUser = (state) => state.user.value;
+export const selectUser = (state) => state.user.user;
+//Create a selector that allows us to grab state from the global store
 
 export default userSlice.reducer;
